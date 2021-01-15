26245215328040ba73ae30497062bbbf3491c37a
Maksim-Vodyanov <maksim.vodyanov@ibm.com>
2021-01-15 15:13:50 +0300

    getting data from REST

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
-import logo from './logo.svg';
-import Custom from './components/Custom'
-import './App.css';
+import "./App.scss";
+import { useEffect, useState } from "react";
+import { Alert, Button, Layout, message, Select, Space, Spin } from "antd";
+import { baseUrl, endpoints } from "./environment";
+
+const { Content } = Layout;
+const { Option } = Select;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState(null);
+  const [content, setContent] = useState();
 
-        <Custom /> 
-      </header>
-    </div>
+  const handleChange = (value) => {
+    setValue(value);
+  };
+
+  const onSubmit = async () => {
+    setLoading(true);
+    fetch(baseUrl + value)
+      .then((reponse) => reponse.text())
+      .then((text) => {
+        setLoading(false);
+        setContent(text);
+      })
+      .catch((error) => {
+        setLoading(false);
+        message.error(error.toString());
+      });
+  };
+
+  useEffect(() => {
+    setContent();
+  }, [value]);
+
+  return (
+    <Spin spinning={loading}>
+      <Layout className="layout">
+        <Content>
+          <div className="site-layout-content">
+            <Space>
+              <Select
+                defaultValue="lucy"
+                onChange={handleChange}
+                value={value}
+                placeholder="Select option"
+                allowClear={false}
+                style={{ width: 200 }}
+              >
+                {Object.keys(endpoints).map((key) => (
+                  <Option value={endpoints[key]} key={key}>
+                    {key}
+                  </Option>
+                ))}
+              </Select>
+              <Button
+                type="primary"
+                onClick={onSubmit}
+                disabled={value == null}
+              >
+                Submit
+              </Button>
+            </Space>
+
+            {content && (
+              <Alert
+                style={{ marginTop: 20 }}
+                message={
+                  <span dangerouslySetInnerHTML={{ __html: content }}></span>
+                }
+                type="info"
+              />
+            )}
+          </div>
+        </Content>
+      </Layout>
+    </Spin>
   );
 }
 
---
+++ src/environment.js
@@ -0,0 +1,7 @@
+export const baseUrl = "http://ocp-ex-pyflask8-s2i-pyflask.tlcog-ocp-cluster2-ed9112eb83c761afd4c566b0882eaa3e-0000.us-east.containers.appdomain.cloud";
+
+export const endpoints = {
+  base: "/",
+  version: "/version",
+  test: "/test",
+};
